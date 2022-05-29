30d911fb72fda2f0ecc7ce00d8783f7cb6509428
KowsarRahman <s3825159@student.rmit.edu.au>
2022-05-29 23:00:04 +1000

    timing feature added

--- btindex.java
+++ btindex.java
@@ -1,9 +1,12 @@
 public class btindex {
 
+    long startTime;
+
     public static void main(String[] args) {
 
         int pageSize = 0;
         String heapfile = null;
+        long startTime = System.currentTimeMillis();
 
         try {
             pageSize = Integer.parseInt(args[1]);
@@ -13,6 +16,10 @@ public class btindex {
             System.exit(0);
         }
         btindex_method(pageSize, heapfile);
+        long endTime = System.currentTimeMillis();
+        long timeInMilliseconds = endTime - startTime;
+        System.out.println("Time taken to load data (milliseconds): " + timeInMilliseconds);
+        System.out.println("Time taken to load data (seconds): " + (timeInMilliseconds / 1000));
     }
 
     public static void btindex_method(int pageSize, String heapfile) {
--- btsearch.java
+++ btsearch.java
@@ -6,6 +6,7 @@ public class btsearch {
         String indexfile = null;
         String range1 = null;
         String range2 = null;
+        long startTime = System.currentTimeMillis();
 
         try {
             heapfile = args[0];
@@ -17,6 +18,10 @@ public class btsearch {
             System.exit(0);
         }
         btsearch_method(heapfile, indexfile, range1, range2);
+        long endTime = System.currentTimeMillis();
+        long timeInMilliseconds = endTime - startTime;
+        System.out.println("Time taken to load data (milliseconds): " + timeInMilliseconds);
+        System.out.println("Time taken to load data (seconds): " + (timeInMilliseconds / 1000));
     }
 
     public static void btsearch_method(String heapfile, String indexfile, String range1, String range2) {
